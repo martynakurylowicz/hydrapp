9740e371ca1d98a37ad7d93f1b0ea4f8c0daf64a
Martyna Kuryłowicz <martynakurylowicz@gmail.com>
2020-07-05 22:48:44 +0200

    js calculations

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,63 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
+
+// const entryTextarea = document.querySelector(".newIdea--js");
+
+// const saveButton = document.querySelector(".save--js");
+
+// saveButton.addEventListener("click", () => {
+//   localStorage.setItem("newIdea", entryTextarea.value);
+// });
+
+// const loadButton = document.querySelector(".load--js");
+
+// loadButton.addEventListener("click", () => {
+//   entryTextarea.value = localStorage.getItem("newIdea");
+// });
+
+const numberGlass = document.querySelector(".number--js");
+
+let newNumber;
+
+const plusGlass = document.querySelector(".plus--js");
+
+const minusGlass = document.querySelector(".minus--js");
+
+const water = document.querySelector(".app__image--water");
+
+const key = new Date().toISOString().slice(0, 10);
+
+plusGlass.addEventListener("click", () => {
+  newNumber = numberGlass.textContent * 1 + 1;
+  numberGlass.textContent = newNumber;
+  localStorage.setItem(key, newNumber);
+  water.style.opacity = localStorage.getItem(key) * 0.2;
+});
+
+minusGlass.addEventListener("click", () => {
+  newNumber = numberGlass.textContent * 1 - 1;
+  if (newNumber >= 0) {
+    numberGlass.textContent = newNumber;
+    localStorage.setItem(key, newNumber);
+    water.style.opacity = localStorage.getItem(key) * 0.2;
+  }
+});
+
+console.log(localStorage.getItem(key));
+
+if (localStorage.getItem(key) == null) {
+  localStorage.setItem(key, 0);
+  water.style.opacity = localStorage.getItem(key) * 0.2;
+}
+
+if (localStorage.getItem(key) >= 0) {
+  numberGlass.textContent = localStorage.getItem(key);
+  water.style.opacity = localStorage.getItem(key) * 0.2;
+}
